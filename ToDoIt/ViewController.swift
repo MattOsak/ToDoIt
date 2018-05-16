d9c56fc40d4ca31ef102ece08ad5389e670f61c6
MattOsak <matt.osak@hotmail.com>
2018-05-16 09:25:44 -0400

    Initial Commit

---
+++ ToDoIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoIt
+//
+//  Created by Matt Osak on 2018-05-16.
+//  Copyright © 2018 Matt Osak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
